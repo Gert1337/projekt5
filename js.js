1be0aeb7b8164e36d67712303379ceb158ad7dd3
Gert1337 <Gert1337@users.noreply.github.com>
2020-04-13 15:43:22 +0200

    opdated til dansk

--- js.js
+++ js.js
@@ -14,9 +14,9 @@ function showTab(n) {
     document.getElementById("prevBtn").style.display = "inline";
   }
   if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
+    document.getElementById("nextBtn").innerHTML = "Indsend";
   } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
+    document.getElementById("nextBtn").innerHTML = "Næste";
   }
   // ... and run a function that displays the correct step indicator:
   fixStepIndicator(n)
