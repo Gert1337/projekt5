65aa3097fe07e8ea2d625a71b992326556b1379e
HiepNguyen10 <60644387+HiepNguyen10@users.noreply.github.com>
2020-04-14 16:08:23 +0200

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,11 +1,3 @@
-<!Doctype html>
-<html lang="da">
-  <head>
-    <meta charset="UTF-8">
-    <meta "viewport" content="width-device-width, initial-scale=1">
-<title>Hej</title>
 <script>
   alert("alert i header");
 </script>
-  </head>
-</html>
